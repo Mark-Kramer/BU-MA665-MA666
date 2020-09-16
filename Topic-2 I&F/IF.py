400bad1e43a3de4b95860b515f06a2a05eaaa210
BFordyce <66751043+BFordyce@users.noreply.github.com>
2020-09-15 20:19:50 -0400

    Update IF.py

--- Topic-2 I&F/IF.py
+++ Topic-2 I&F/IF.py
@@ -15,9 +15,12 @@ def IF(I,C,plot):                #Input important parameters, and an option to p
     for k in range(1,999):       #March forward in time,
         V[k+1] = V[k] + dt*(I/C) #Update the voltage,
         if V[k+1] > Vth:         #... and check if the voltage exceeds the threshold.
+            spikes=V[k+1]>Vth
             V[k+1] = Vreset      #... if so, reset the voltage
             
     t = np.arange(0,len(V))*dt   #Define the time axis.
+    
+    return spikes/t
     
     if plot:                     #If the plot flag is on, plot V vs t.
         plt.plot(t,V)
