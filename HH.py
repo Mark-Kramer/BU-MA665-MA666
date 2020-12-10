f717d58477d4ad2ab8601342bc2098ce585abdc4
taijiayue1997 <44779618+taijiayue1997@users.noreply.github.com>
2020-12-10 12:33:16 -0500

    Add files via upload

--- HH.py
+++ HH.py
@@ -49,17 +49,20 @@ def singleTrial(I0,T0,gM=0):
     m = np.zeros([T,1])
     h = np.zeros([T,1])
     n = np.zeros([T,1])
+    mKM = np.zeros([T,1])
 
     V[0]=-70.0
     m[0]=0.05
     h[0]=0.54
     n[0]=0.34
+    mKM[0] = 0
 
     for i in range(0,T-1):
-        V[i+1] = V[i] + dt*(gNa0*m[i]**3*h[i]*(ENa-(V[i]+65)) + gK0*n[i]**4*(EK-(V[i]+65)) + gL0*(EL-(V[i]+65)) + I0[i]);
+        V[i+1] = V[i] + dt*(-gM*mKM[i]*(V[i]+95) + gNa0*m[i]**3*h[i]*(ENa-(V[i]+65)) + gK0*n[i]**4*(EK-(V[i]+65)) + gL0*(EL-(V[i]+65)) + I0[i]);
         m[i+1] = m[i] + dt*(alphaM(V[i])*(1-m[i]) - betaM(V[i])*m[i]);
         h[i+1] = h[i] + dt*(alphaH(V[i])*(1-h[i]) - betaH(V[i])*h[i]);
         n[i+1] = n[i] + dt*(alphaN(V[i])*(1-n[i]) - betaN(V[i])*n[i]);
+        mKM[i+1] = mKM[i] + dt*(alphaKM(V[i])*(1-mKM[i]) - betaKM(V[i])*mKM[i])
     return V,m,h,n,t
 
 
@@ -95,10 +98,10 @@ def alphaN(V):
 def betaN(V):
     return 0.125*np.exp(-(V+65)/80)
 
-def alphaM(V):
+def alphaKM(V):
     return 1.5 * 0.02 / (1 + np.exp((-V-20)/5))
 
-def betaM(V):
+def betaKM(V):
     return 1.25* 0.01 * np.exp((-V-43)/18)
 
 
