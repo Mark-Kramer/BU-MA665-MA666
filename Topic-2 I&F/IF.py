aca06ad3e96bef95a85a03ecac2b2250552f501d
williamc96 <89996611+williamc96@users.noreply.github.com>
2021-09-09 13:46:57 -0400

    Revert "fixing range to allow initial condition to work"
    This reverts commit 53459ac40d96d5a12e64004160a8f3ef76518a9a.

--- Topic-2 I&F/IF.py
+++ Topic-2 I&F/IF.py
@@ -5,19 +5,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def IF(I,C,plot):                #Input important parameters, and an option to plot V vs t.
-
+    
     Vth = 1;                     #Define the voltage threshold.
     Vreset = 0;                  #Define the reset voltage.
     dt=0.01                      #Set the timestep.
     V = np.zeros([1000,1])       #Initialize V.
     V[0]=0.2;                    #Set the initial condition.
 
-    for k in range(0,999):       #March forward in time,
+    for k in range(1,999):       #March forward in time,
         V[k+1] = V[k] + dt*(I/C) #Update the voltage,
         if V[k+1] > Vth:         #... and check if the voltage exceeds the threshold.
             V[k+1] = Vreset      #... if so, reset the voltage
-
+            
     t = np.arange(0,len(V))*dt   #Define the time axis.
-
+    
     if plot:                     #If the plot flag is on, plot V vs t.
         plt.plot(t,V)
